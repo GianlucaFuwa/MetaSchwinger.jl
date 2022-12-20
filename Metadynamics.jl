e212339a2e8453f6bcd26f601098a65606d21594
GianlucaFuwa <51165364+GianlucaFuwa@users.noreply.github.com>
2022-12-21 00:34:12 +0100

    Update Metadynamics.jl

--- Metadynamics.jl
+++ Metadynamics.jl
@@ -1,8 +1,10 @@
+### Function to find point on potential grid for given Q ###
 function grid_ind(q,dq,Q_max)
     grid_index = (q+Q_max)/dq+0.5
     return round(Int,grid_index)
 end;
 
+### Function to update bias potentia for given Q ###
 function update_bias!(bias_potential,q,Q_max,dq,w)
     grid_index = grid_ind(q,dq,Q_max)
     grid_q = -Q_max+(grid_index-1)*dq
