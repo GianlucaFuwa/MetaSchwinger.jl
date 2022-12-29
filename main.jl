f60f779a8c359c79d4151b86b3aa42dacca2a222
GianlucaFuwa <51165364+GianlucaFuwa@users.noreply.github.com>
2022-12-29 13:50:55 +0100

    Update main.jl

--- main.jl
+++ main.jl
@@ -4,37 +4,44 @@ include("TopCharge.jl")
 include("ContCharge.jl")
 include("Metadynamics.jl")
 include("pretty_plots.jl")
+include("error_est.jl")
 
 using Plots
 using Statistics
 using StatsBase
 using FFTW
 using LaTeXStrings
+using JLD
 
-const N_s = 32   #Definiere Theorie-Parameter (Anzahl an Zeit-cuts, und Potenzialkonstante)
+### Physics Parameters ###
+const N_s = 32 
 const N_t = 32
 const d = 2
 const β = N_s*N_t/80
-const ε = 0.2
-const Q_max = 15
-const Q_thresh = 12
+### Metadynamics Parameters ###
+const ε = 0.2 #Metropolis step-size
+const Q_max = 10
+const Q_thresh = 8
 const dq = 0.001
-const w = 1e-3
+const w = 1e-4
 const k = 1000
+### Simulation Parameters ###
+const N_therm = 50000
+const N_sweeps = 1000000
 const n_skip = 10
 
-const N_therm = 50000
-const N_sweeps = 2000000   #Anzahl an Metropolis-Sweeps in der Simulation
 boundary_count = 0
 acc = 0
 
-start = zeros(N_s,N_t,d).*(2*pi);  #Startkonfiguration
-bias_potential = zeros(grid_ind(Float64(Q_max))); #Bias-Potential Grid
-q_vals = range(-Q_max,Q_max,length(bias_potential));
+start = zeros(N_s,N_t,d);  #Startconfiguration
 
-Q_top,Q_cont,Q2_weighted = metropolis_meta(start,bias_potential);
-#configs = metropolis(N_sweeps,N_therm,N_s,N_t,beta,epsilon)
+#bias_potential = zeros(grid_ind(Float64(Q_max))); #Bias-Potential Grid
+q_vals = range(-Q_max,Q_max-dq,length(bias_potential));
 
-χ_top = sum(Q2_weighted)
+Q_top,Q_cont,Q2_weighted,bias_potential = metropolis_meta(start,bias_potential,false); #Build Bias-Potential
+#Q_top,Q_cont,Q2_weighted,bias_potential = metropolis_meta(start,bias_potential,true); äSimulate with static Bias-Potential (or dynammic if preferred)
+
+χ_top,δχ_top = bootstrap(Q2_weighted,1000,true)
+χ_top = round(χ_top,digits=3)
 
 pretty_plots(q_vals,bias_potential,Q_cont,Q_top)
